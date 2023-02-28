2419fe95fd3a00af930e50ec620eebe1dbedd132
a-shiro <alekmitov20@gmail.com>
2023-02-28 10:45:17 +0200

    Added Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+export const Home = () => {
+  return (
+    <div>
+      <nav>
+        <img
+          src="https://hips.hearstapps.com/hmg-prod/images/2019-honda-civic-sedan-1558453497.jpg?resize=480:*"
+          height="100px"
+          width="150px"
+        />
+        <div style={{ display: "inline-block" }}>
+          <a>Home</a>
+          <a>Cars</a>
+          <a>About</a>
+          <a>Contacts</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
