028b7e1e8db14e5ee6e8f0e25c8cbfb4e65239ed
a-shiro <alekmitov20@gmail.com>
2023-03-01 18:23:54 +0200

    Add: Routes Manager

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home/Home";
 import { Nav } from "./common/Navigation";
 import { Footer } from "./common/Footer";
-import { TermsAndConditions } from "./pages/tac/TermsAndConditions";
-import { PageNotFound } from "./pages/404/404";
-import { About } from "./pages/about/About";
-import { Contact } from "./pages/contact/Contact";
-import { Catalog } from "./pages/catalog/Catalog";
-import { RentCar } from "./pages/catalog/RentCar";
-import { Profile } from "./pages/profile/Profile";
-import { Login } from "./pages/login/Login";
-import { Register } from "./pages/register/Register";
+import { BrowserRouter } from "react-router-dom";
+import { RoutesManager } from "./setup/routes-manager/RoutesManager";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cars" element={<Catalog />} />
-          <Route path="/whatever-car-and-model" element={<RentCar />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/terms-and-conditions"
-            element={<TermsAndConditions />}
-          />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <RoutesManager />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/setup/routes-manager/RoutesManager.js
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../../pages/home/Home";
+import { TermsAndConditions } from "../../pages/tac/TermsAndConditions";
+import { PageNotFound } from "../../pages/404/404";
+import { About } from "../../pages/about/About";
+import { Contact } from "../../pages/contact/Contact";
+import { Catalog } from "../../pages/catalog/Catalog";
+import { RentCar } from "../../pages/catalog/RentCar";
+import { Profile } from "../../pages/profile/Profile";
+import { Login } from "../../pages/login/Login";
+import { Register } from "../../pages/register/Register";
+
+export const RoutesManager = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cars" element={<Catalog />} />
+      <Route path="/whatever-car-and-model" element={<RentCar />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+};
