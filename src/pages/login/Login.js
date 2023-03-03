2e9d0d4fccfe3b4ec2ff4f83bf483f3ce58e8221
a-shiro <alekmitov20@gmail.com>
2023-03-03 12:59:15 +0200

    Add: Login Functionality

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,14 +1,35 @@
+import { auth } from "../../setup/config/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const logIn = async () => {
+    await signInWithEmailAndPassword(auth, email, password);
+  };
+
   return (
     <div>
-      <h1>Login</h1>
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
-      </form>
+      <h1>Welcome! Please sign in.</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={logIn}>Sign in</button>
+
       <p>
         Dont have an account? <Link to="/register">Register here.</Link>
       </p>
