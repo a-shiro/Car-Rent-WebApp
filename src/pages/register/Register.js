174293e50ebda80ad1b3c6e5508649f78b0d0cd5
a-shiro <alekmitov20@gmail.com>
2023-03-03 12:51:47 +0200

    Add: Register Functionality

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,17 +1,57 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../setup/config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div>
       <h1>Register</h1>
-      <form>
-        <input type="text" placeholder="First Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Repeat Password" />
+
+      <form onSubmit={registerUser}>
+        <input
+          type="text"
+          required
+          placeholder="First Name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="email"
+          required
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          required
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          required
+          placeholder="Confirm Password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
 
         <button>Register</button>
       </form>
+
       <p>
         Have an account already? <Link to="/login">Login here.</Link>
       </p>
