9bf3ed478cde3250c300e70599b9204521ce6e9c
a-shiro <alekmitov20@gmail.com>
2023-03-23 09:33:52 +0200

    Add: Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Nav } from "./common/Navigation/Navigation";
-import { Footer } from "./common/Footer";
+import { Footer } from "./common/Footer/Footer";
 import { RoutesManager } from "./setup/routes-manager/RoutesManager";
 import { BrowserRouter } from "react-router-dom";
 
---
+++ src/common/Footer/Footer.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div className={styles.Container}>
+        <div>
+          <p className={styles.ContainerTitle}>Contacts:</p>
+          <p>+359 88 888 8888</p>
+          <p>rentx@email.com</p>
+        </div>
+        <div>
+          <p className={styles.ContainerTitle}>Social Networking:</p>
+          <p>Instagram - @official_rentx</p>
+          <p>Facebook - RentX Cars</p>
+          <p>TikTok - @official_rentx</p>
+        </div>
+        <div>
+          <p className={styles.ContainerTitle}>Links:</p>
+          <Link to="/terms-and-conditions">Terms And Conditions</Link>
+        </div>
+      </div>
+      <div>
+        <p className={styles.CopyrightText}>
+          Copyright © 2023 RentX. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
