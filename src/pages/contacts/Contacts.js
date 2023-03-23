8cce0167c6fb8f24c054d31e23d23ec74395682d
a-shiro <alekmitov20@gmail.com>
2023-03-23 10:17:25 +0200

    Add: Styling

--- src/pages/contacts/Contacts.js
+++ src/pages/contacts/Contacts.js
@@ -3,17 +3,36 @@ import vector from "../../assets/images/car-vector.png";
 
 export const Contacts = () => {
   return (
-    <div>
+    <div className={styles.MainContainer}>
       <header className={styles.Header}>
-        <h1>Our Contacts</h1>
+        <h1>- Our Contacts -</h1>
       </header>
-      <div>
-        <img src={vector} className={styles.VectorImage} />
+      <div className={styles.Container}>
         <div>
-          <p>Phone: 000000000</p>
-          <p>Email: example@email.com</p>
-          <p>Office address: --------</p>
+          <div className={styles.PhoneInfoContainer}>
+            <p className={styles.TitleText}>Phone</p>
+            <p> +359 88 888 8888</p>
+          </div>
+          <div>
+            <p className={styles.TitleText}>Email </p>
+            <p>rentx@email.com</p>
+          </div>
         </div>
+        <div>
+          <p className={styles.TitleText}>Office address (Main)</p>
+          <p>California, San Francisco</p>
+          <p>94016</p>
+          <p>We work 24/7</p>
+        </div>
+        <div>
+          <p className={styles.TitleText}>Office address (Secondary)</p>
+          <p>Nevada, Las Vegas</p>
+          <p>88901</p>
+          <p>We work 24/7</p>
+        </div>
+      </div>
+      <div className={styles.ImageContainer}>
+        <img src={vector} className={styles.VectorImage} />
       </div>
     </div>
   );
