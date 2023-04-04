3d7e22f46464da761dbd3f824e71cc975523c877
a-shiro <alekmitov20@gmail.com>
2023-04-04 11:26:40 +0300

    Add: Rerender upon deleting doc

--- src/pages/administration/components/CollectionTable/components/TableBody/components/DeleteButton.js
+++ src/pages/administration/components/CollectionTable/components/TableBody/components/DeleteButton.js
@@ -1,9 +1,17 @@
+import { useContext } from "react";
 import { deleteDataById } from "../../../../../../../services/queries";
+import { CarsContext } from "../../../../../Administration";
+import { getData } from "../../../../../../../services/queries";
 import styles from "../../../../../Administration.module.css";
 
 export const DeleteButton = ({ id }) => {
+  const [carsList, setCarsList] = useContext(CarsContext);
+
   const deleteCar = async () => {
     await deleteDataById("cars", id);
+
+    const data = await getData("cars");
+    setCarsList(data);
   };
 
   return (
