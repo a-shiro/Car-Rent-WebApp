48ccee698af77755c1f4b3e4f02922ffdde2a859
a-shiro <alekmitov20@gmail.com>
2023-04-07 11:04:25 +0300

    Add: Brands query

--- src/pages/home/components/Brands/Brands.js
+++ src/pages/home/components/Brands/Brands.js
@@ -1,22 +1,30 @@
 import { Card } from "./Card";
 import styles from "./Brands.module.css";
+import { useEffect, useState } from "react";
+import { getData } from "../../../../services/queries";
 
 export const Brands = () => {
+  const [brandsList, setBrandsList] = useState([]);
+
+  useEffect(() => {
+    const queryBrands = async () => {
+      const brands = await getData("brands");
+
+      setBrandsList(brands);
+    };
+
+    queryBrands();
+  }, []);
+
   return (
     <section className={styles.BrandsSection}>
       <header className={styles.Header}>
         <h1>- Brands -</h1>
       </header>
       <div className={styles.CardsContainer}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {brandsList.map((brand) => (
+          <Card key={brand.id} data={brand} />
+        ))}
       </div>
     </section>
   );
--- src/pages/home/components/Brands/Card.js
+++ src/pages/home/components/Brands/Card.js
@@ -1,12 +1,12 @@
 import styles from "./Brands.module.css";
 
-export const Card = () => {
+export const Card = ({ data }) => {
   return (
     <div className={styles.Card}>
       <img
         className={styles.CardImage}
-        src="https://1000logos.net/wp-content/uploads/2018/03/Honda-logo.png"
-        alt="brand-logo"
+        src={data.imgUrl}
+        alt={`${data.name}-logo`}
       />
     </div>
   );
