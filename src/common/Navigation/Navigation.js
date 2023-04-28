ff05edfa51267988f6a7ce188fb3ec69a012d44e
a-shiro <alekmitov20@gmail.com>
2023-04-28 17:55:49 +0300

    Refactor: Navbar styling

--- src/common/Navigation/Navigation.js
+++ src/common/Navigation/Navigation.js
@@ -1,12 +1,42 @@
-import { NavLinks } from "./components/NavLinks";
-import { NavBrand } from "./components/NavBrand";
-import styles from "./Navigation.module.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 export const Nav = () => {
+  const [navListElement, setNavListElement] = useState(null);
+
+  useEffect(() => {
+    setNavListElement(document.getElementsByClassName("navbar-list")[0]);
+  }, []);
+
+  const toggleHandler = () => {
+    navListElement.classList.toggle("active");
+  };
+
   return (
-    <nav className={styles.Navbar}>
-      <NavBrand />
-      <NavLinks />
+    <nav className="navbar">
+      <div className="brand-title">RentX</div>
+      <a href="#" className="toggle-button" onClick={toggleHandler}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+      <div className="links-container">
+        <ul className="navbar-list">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cars">Cars</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
