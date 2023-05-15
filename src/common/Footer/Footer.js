724e2ffa37ae9eac988eda557745b4b2a601e75d
a-shiro <alekmitov20@gmail.com>
2023-05-15 09:52:37 +0300

    Refactor: Styling, responsiveness

--- src/common/Footer/Footer.js
+++ src/common/Footer/Footer.js
@@ -1,30 +1,29 @@
-import { Link } from "react-router-dom";
-import styles from "./Footer.module.css";
+import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer>
-      <div className={styles.Container}>
+      <div className="info-wrapper">
         <div>
-          <p className={styles.ContainerTitle}>Contacts:</p>
+          <h6 className="info-title">Contacts:</h6>
           <p>+359 88 888 8888</p>
           <p>rentx@email.com</p>
         </div>
         <div>
-          <p className={styles.ContainerTitle}>Social Networking:</p>
+          <h6 className="info-title">Social Networking:</h6>
           <p>Instagram - @official_rentx</p>
           <p>Facebook - RentX Cars</p>
           <p>TikTok - @official_rentx</p>
         </div>
         <div>
-          <p className={styles.ContainerTitle}>Links:</p>
-          <a>Terms And Conditions</a>
+          <h6 className="info-title">Links:</h6>
+          <p>Terms And Conditions</p>
+          <p>Trustpilot Reviews</p>
+          <p>FAQ</p>
         </div>
       </div>
-      <div>
-        <p className={styles.CopyrightText}>
-          Copyright © 2023 RentX. All rights reserved
-        </p>
+      <div className="copyright-wrapper">
+        <p>Copyright © 2023 RentX. All rights reserved</p>
       </div>
     </footer>
   );
