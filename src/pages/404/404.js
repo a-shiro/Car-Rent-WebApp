6a194df25e33179d665194914613eec81bb2aad3
a-shiro <alekmitov20@gmail.com>
2023-06-15 09:10:33 +0300

    Refactor: 404 page

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,31 +1,25 @@
 import { Link } from "react-router-dom";
-import styles from "./404.module.css";
-import vector from "../../assets/images/car-vector.png";
+import "./404.css";
+import carVector from "../../assets/images/car-vector.png";
 
 export const PageNotFound = () => {
   return (
-    <div className={styles.MainContainer}>
-      <header className={styles.Header}>
-        <h1>- Page not found -</h1>
-      </header>
-      <div className={styles.Container}>
+    <main>
+      <div className="wrapper-404">
+        <header className="title-wrapper-404">
+          <h1>404</h1>
+          <p>Sorry, but the page you requested was not found.</p>
+        </header>
+
+        <div className="button-wrapper-404">
+          <Link to="/">Back Home</Link>
+          <Link to="/cars">Browse Cars</Link>
+        </div>
+
         <div>
-          <p className={styles.Text}>
-            Sorry, but the page you requested was not found.
-          </p>
-        </div>
-        <div className={styles.ButtonsContainer}>
-          <Link to="/" className={styles.Button}>
-            Back Home
-          </Link>
-          <Link to="/cars" className={styles.Button}>
-            Browse Cars
-          </Link>
+          <img className="vector-image-404" src={carVector} alt="car-vector" />
         </div>
       </div>
-      <div className={styles.ImageContainer}>
-        <img src={vector} className={styles.VectorImage} />
-      </div>
-    </div>
+    </main>
   );
 };
