df210199250c1a289ae9a02dd99619874ffefee0
a-shiro <alekmitov20@gmail.com>
2023-07-01 09:16:22 +0300

    Refactor: Cleaned up code

--- src/pages/home/components/Hero/Hero.js
+++ src/pages/home/components/Hero/Hero.js
@@ -4,29 +4,23 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <section className="hero-section">
-      <div className="container">
-        <div className="hero-title-container">
-          <h1 className="hero-title">
-            ​ELEVATE YOUR JOURNEY WITH LUXURY CAR RENTALS
-          </h1>
-          <p className="hero-subtitle">
+    <section className="section-hero">
+      <div className="container-hero">
+        <div className="title-container-hero">
+          <h1>​ELEVATE YOUR JOURNEY WITH LUXURY CAR RENTALS</h1>
+          <p>
             Indulge in the finest automotive luxury with our premium car rental
             services. Choose from an exquisite fleet of high-end vehicles and
             experience the unmatched comfort, performance, and style that define
             true sophistication.
           </p>
-          <div className="cta-container">
-            <Link to="/cars" className="cta-button">
-              Discover
-            </Link>
-            <Link to="/about" className="cta-button">
-              About
-            </Link>
+          <div className="cta-container-hero">
+            <Link to="/cars">Discover</Link>
+            <Link to="/about">About</Link>
           </div>
         </div>
 
-        <img className="hero-image" src={heroImage} />
+        <img className="image-hero" src={heroImage} />
       </div>
     </section>
   );
