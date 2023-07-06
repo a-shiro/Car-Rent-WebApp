61b19b5d48d5ae3cbc631ce952a37f81815e8902
a-shiro <alekmitov20@gmail.com>
2023-07-06 10:58:24 +0300

    Add: Loading skeleton

---
+++ src/pages/car-catalog/components/CardSkeleton.js
@@ -0,0 +1,9 @@
+export const CardSkeleton = () => {
+  return (
+    <div className="card-skeleton-catalog">
+      <div className="card-checkers"></div>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
--- src/pages/car-catalog/components/CardsWrapper.js
+++ src/pages/car-catalog/components/CardsWrapper.js
@@ -1,23 +1,47 @@
+import { useEffect, useState, Fragment } from "react";
 import { Link } from "react-router-dom";
+import { CardSkeleton } from "./CardSkeleton";
 import "../Catalog.css";
 
 export const CardsWrapper = ({ dataList }) => {
+  const [skeletonVisible, setSkeletonVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSkeletonVisible(false);
+    }, 800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="cards-wrapper-catalog">
-      {dataList.map((car) => {
-        return (
-          <Link to={`${car.urlPath}`} key={car.id}>
-            <div className="card-catalog">
-              <div className="card-checkers"></div>
-              <span>
-                {car.brand} {car.model}
-              </span>
-              <span>starting at {car.price}$</span>
-              <img src={car.thumbnail} alt="car-thumbnail" />
-            </div>
-          </Link>
-        );
-      })}
+      {dataList.length > 0 ? (
+        <Fragment>
+          {dataList.map((car) => {
+            return (
+              <Fragment key={car.id}>
+                {!skeletonVisible ? (
+                  <Link to={`${car.urlPath}`}>
+                    <div className="card-catalog">
+                      <div className="card-checkers"></div>
+                      <span>
+                        {car.brand} {car.model}
+                      </span>
+                      <span>starting at {car.price}$</span>
+                      <img src={car.thumbnail} alt="car-thumbnail" />
+                    </div>
+                  </Link>
+                ) : (
+                  <CardSkeleton />
+                )}
+              </Fragment>
+            );
+          })}
+        </Fragment>
+      ) : (
+        <h1>Unfortunately, no cars were found for the specified filter.</h1>
+      )}
     </div>
   );
 };
