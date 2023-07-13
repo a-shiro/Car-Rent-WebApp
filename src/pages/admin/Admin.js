b7705331f26bed000ac297c4d3d1784c55663c56
a-shiro <alekmitov20@gmail.com>
2023-07-13 12:59:05 +0300

    Add: db POST request for images and collections

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -3,72 +3,103 @@ import { getData } from "../../services/queries";
 import { Table } from "./components/Table";
 import { TableActions } from "./components/TableActions";
 import { Modal } from "./components/Modal";
-import { camelize } from "./utils/camelize";
+import { storage } from "../../setup/config/firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { Dropdown } from "./components/Dropdown";
 import "./Admin.css";
 
 const Admin = () => {
   const [activeCollection, setActiveCollection] = useState("cars");
   const [tableData, setTableData] = useState(null);
-  const [tableHeads, setTableHeads] = useState(null);
-  const [collectionOptions, setCollectionOptions] = useState(null);
+
+  const sortData = (obj) =>
+    Object.keys(obj)
+      .sort()
+      .reduce((res, key) => ((res[key] = obj[key]), res), {});
 
   useEffect(() => {
-    const sortData = (tableHeadsData, collectionData) => {
-      const sortedRows = [];
-
-      for (const obj of collectionData) {
-        const tableRow = [];
-
-        for (const index in Object.entries(obj)) {
-          const key = camelize(tableHeadsData[index]);
-          tableRow.push(obj[key]);
-        }
-
-        tableRow.push(<TableActions />);
-        sortedRows.push(tableRow);
-      }
-
-      return sortedRows;
-    };
-
     const queryData = async () => {
-      const collectionData = await getData(activeCollection);
-      let tableHeadsData = await getData("tableHeads");
-      let optionsData = tableHeadsData[0];
+      let data = await getData(activeCollection);
 
-      tableHeadsData = tableHeadsData[0][activeCollection];
+      if (data) {
+        data = data.map(sortData);
 
-      delete optionsData.id;
-      optionsData = Object.keys(optionsData);
-
-      return [collectionData, tableHeadsData, optionsData];
+        setTableData(data);
+      }
     };
 
-    const prepareTable = async () => {
-      const [collectionData, tableHeadsData, optionsData] = await queryData();
-      const sortedTableData = sortData(tableHeadsData, collectionData);
-
-      setCollectionOptions(optionsData);
-      setTableHeads(tableHeadsData);
-      setTableData(sortedTableData);
-    };
-
-    prepareTable();
+    queryData();
   }, [activeCollection]);
 
   return (
     <main>
-      <Table
-        activeCollection={activeCollection}
-        setActiveCollection={setActiveCollection}
-        tableHeads={tableHeads}
-        tableData={tableData}
-        collectionOptions={collectionOptions}
-      />
+      <section className="table-section-admin">
+        <div className="table-title-wrapper-admin">
+          <h1>
+            Active collection: <span>{activeCollection}</span>
+          </h1>
+        </div>
 
-      <Modal tableHeads={tableHeads} tableData={tableData} />
+        <Table data={tableData} activeCollection={activeCollection} />
+
+        <Modal data={tableData} activeCollection={activeCollection} />
+
+        <Dropdown
+          activeCollection={activeCollection}
+          setActiveCollection={setActiveCollection}
+        />
+      </section>
     </main>
   );
+
+  // const [image, setImage] = useState(null);
+  // const [url, setUrl] = useState(null);
+  // const [model, setModel] = useState("gt3");
+
+  // const handleChange = (e) => {
+  //   if (e.target.files[0]) {
+  //     setImage(e.target.files[0]);
+  //   }
+  // };
+
+  // const addData = (e) => {
+  //   e.preventDefault();
+  //   const storageRef = ref(storage, `cars/${model}/${image.name}`);
+  //   const uploadTask = uploadBytesResumable(storageRef, image);
+
+  //   uploadTask.on(
+  //     "state_changed",
+  //     (snapshot) => {
+  //       const progress =
+  //         (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  //       console.log("Upload is " + progress + "% done");
+  //       switch (snapshot.state) {
+  //         case "paused":
+  //           console.log("Upload is paused");
+  //           break;
+  //         case "running":
+  //           console.log("Upload is running");
+  //           break;
+  //       }
+  //     },
+  //     (error) => {},
+  //     () => {
+  //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+  //         setUrl(downloadURL);
+  //       });
+  //     }
+  //   );
+  // };
+
+  // return (
+  //   <form onSubmit={addData}>
+  //     <input type="file" onChange={handleChange}></input>
+  //     <input type="submit" />
+  //     <h1>{url}</h1>
+  //     <img src={url} />
+  //   </form>
+  // <CarsCollectionForm />
+  // );
 };
 
 export default Admin;
--- src/pages/admin/components/Modal.js
+++ src/pages/admin/components/Modal.js
@@ -1,62 +1,165 @@
 import { useEffect, useState } from "react";
+import { postData } from "../../../services/queries";
+import { storage } from "../../../setup/config/firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
-export const Modal = ({ tableHeads, tableData }) => {
+export const Modal = ({ data, activeCollection }) => {
   const [formFields, setFormFields] = useState(null);
-
-  const changeHandler = (e) => {
-    console.log(e.target);
-  };
+  const [fieldValues, setFieldValues] = useState(null);
+  const [singleImageData, setSingleImageData] = useState(null);
+  const [multipleImages, setMultipleImages] = useState([]);
+  const [doneUploading, setDoneUploading] = useState(false);
 
   useEffect(() => {
-    const createFormSchema = (fields, values) => {
-      const schema = [];
+    const createFormSchema = (data) => {
+      const schema = {};
 
-      fields.forEach((name, index) => {
-        const fieldName = name;
-        let dataType = typeof values[index];
-
-        // ToDo: Add a way to send files to database
-        if (dataType === "object") {
-          dataType = "string";
+      Object.entries(data[0]).map(([key, value]) => {
+        if (key === "id") {
+          return;
         }
-        // /\
-        // |________________________
-        // Fix dataType to be file |
 
-        schema.push([fieldName, dataType]);
+        const fieldName = key;
+        let fieldType = typeof value;
+
+        if (fieldName === "photos" || fieldName === "thumbnail") {
+          fieldType = "file";
+        }
+        schema[fieldName] = fieldType;
       });
 
       setFormFields(schema);
     };
 
-    if (tableHeads && tableData) {
-      const fields = tableHeads.slice(0, tableHeads.length - 2);
-      const values = tableData[0].slice(0, tableHeads.length - 2);
-
-      createFormSchema(fields, values);
+    if (data !== null && data.length > 0) {
+      createFormSchema(data);
     }
-  }, [tableHeads]);
+
+    // WRITE A CLEAN CODE
+
+    if (doneUploading && fieldValues[singleImageData.fieldName] !== "") {
+      console.log("posting here");
+
+      postData(activeCollection, fieldValues);
+    }
+  }, [data, doneUploading, fieldValues]);
+
+  const onValueChange = (e) => {
+    let newValue = e.target.value;
+
+    if (e.target.placeholder === "photos") {
+      setMultipleImages(Array.from(e.target.files));
+      newValue = [];
+    } else if (e.target.type === "file") {
+      setSingleImageData({
+        fieldName: e.target.placeholder,
+        file: e.target.files[0],
+      });
+      newValue = "";
+    }
+
+    if (e.target.type === "number") {
+      newValue = Number(newValue);
+    }
+
+    setFieldValues(() => ({
+      ...fieldValues,
+      [e.target.placeholder]: newValue,
+    }));
+  };
+
+  const createFields = (key) => {
+    let multipleFiles = false;
+    let isTextArea = false;
+
+    switch (key) {
+      case "photos":
+        multipleFiles = true;
+        break;
+
+      case "description":
+      case "countries":
+        isTextArea = true;
+        break;
+    }
+
+    return isTextArea ? (
+      <textarea
+        onChange={onValueChange}
+        placeholder={key}
+        required
+        key={key}
+      ></textarea>
+    ) : (
+      <input
+        onChange={onValueChange}
+        placeholder={key}
+        type={formFields[key]}
+        multiple={multipleFiles}
+        required
+        key={key}
+      />
+    );
+  };
+
+  const closeModal = () => {
+    document.getElementById("modal").classList.toggle("visible-admin");
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const uploadImage = async () => {
+      if (singleImageData !== null) {
+        const file = singleImageData.file;
+        const fieldName = singleImageData.fieldName;
+        const filePath = `${fieldName}/${file.name}`;
+        const storageRef = ref(storage, filePath);
+
+        await uploadBytes(storageRef, file);
+        const fileUrl = await getDownloadURL(storageRef);
+
+        setFieldValues(() => ({
+          ...fieldValues,
+          [fieldName]: fileUrl,
+        }));
+      }
+
+      if (multipleImages.length > 0) {
+        multipleImages.map(async (file) => {
+          const filePath = `cars/${fieldValues.model.toLowerCase()}/${
+            file.name
+          }`;
+
+          const storageRef = ref(storage, filePath);
+
+          await uploadBytes(storageRef, file);
+
+          const fileUrl = await getDownloadURL(storageRef);
+
+          fieldValues.photos.push(fileUrl);
+
+          if (multipleImages.length === fieldValues.photos.length) {
+            setDoneUploading(true);
+          }
+        });
+      }
+    };
+
+    uploadImage();
+  };
 
   return (
     <div id="modal" className="modal-admin">
       <div className="form-wrapper-admin">
+        <button onClick={closeModal} className="close-modal-admin">
+          X
+        </button>
         <h1>Add</h1>
         {formFields && (
-          <form className="form-admin">
-            {formFields.map(([name, type]) => (
-              <label htmlFor={name.toLowerCase()} key={name}>
-                <input
-                  value=""
-                  onChange={changeHandler}
-                  id={name.toLowerCase()}
-                  type={type}
-                  placeholder={name}
-                />
-              </label>
-            ))}
-            <div>
-              <input type="submit" />
-            </div>
+          <form onSubmit={submitHandler} className="form-admin">
+            {Object.keys(formFields).map(createFields)}
+            <button className="form-submit-admin">Submit</button>
           </form>
         )}
       </div>
