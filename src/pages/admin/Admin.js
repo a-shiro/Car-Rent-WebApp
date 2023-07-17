c256278a6630f0e9b35cdbf5d5811693221669d2
a-shiro <alekmitov20@gmail.com>
2023-07-17 12:06:58 +0300

    ---

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getData } from "../../services/queries";
 import { Table } from "./components/Table";
-import { TableActions } from "./components/TableActions";
 import { Modal } from "./components/Modal";
-import { storage } from "../../setup/config/firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { Dropdown } from "./components/Dropdown";
 import "./Admin.css";
 
@@ -51,55 +48,6 @@ const Admin = () => {
       </section>
     </main>
   );
-
-  // const [image, setImage] = useState(null);
-  // const [url, setUrl] = useState(null);
-  // const [model, setModel] = useState("gt3");
-
-  // const handleChange = (e) => {
-  //   if (e.target.files[0]) {
-  //     setImage(e.target.files[0]);
-  //   }
-  // };
-
-  // const addData = (e) => {
-  //   e.preventDefault();
-  //   const storageRef = ref(storage, `cars/${model}/${image.name}`);
-  //   const uploadTask = uploadBytesResumable(storageRef, image);
-
-  //   uploadTask.on(
-  //     "state_changed",
-  //     (snapshot) => {
-  //       const progress =
-  //         (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-  //       console.log("Upload is " + progress + "% done");
-  //       switch (snapshot.state) {
-  //         case "paused":
-  //           console.log("Upload is paused");
-  //           break;
-  //         case "running":
-  //           console.log("Upload is running");
-  //           break;
-  //       }
-  //     },
-  //     (error) => {},
-  //     () => {
-  //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-  //         setUrl(downloadURL);
-  //       });
-  //     }
-  //   );
-  // };
-
-  // return (
-  //   <form onSubmit={addData}>
-  //     <input type="file" onChange={handleChange}></input>
-  //     <input type="submit" />
-  //     <h1>{url}</h1>
-  //     <img src={url} />
-  //   </form>
-  // <CarsCollectionForm />
-  // );
 };
 
 export default Admin;
--- src/pages/admin/components/Modal.js
+++ src/pages/admin/components/Modal.js
@@ -148,6 +148,8 @@ export const Modal = ({ data, activeCollection }) => {
         setMultipleFilesUploaded(true);
       }
     });
+
+    closeModal();
   };
 
   return (
