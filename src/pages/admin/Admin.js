a222ea7e83a84bd75a6c00bee762ed6909a2ef2d
a-shiro <alekmitov20@gmail.com>
2023-07-18 11:50:01 +0300

    Add: Access restriction

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -3,11 +3,14 @@ import { getData } from "../../services/queries";
 import { Table } from "./components/Table";
 import { Modal } from "./components/Modal";
 import { Dropdown } from "./components/Dropdown";
+import { validCodes } from "./codes.js";
 import "./Admin.css";
 
 const Admin = () => {
   const [activeCollection, setActiveCollection] = useState("cars");
   const [tableData, setTableData] = useState(null);
+  const [userInput, setUserInput] = useState("");
+  const [accessGranted, setAcessGranted] = useState(false);
 
   const sortData = (obj) =>
     Object.keys(obj)
@@ -26,26 +29,48 @@ const Admin = () => {
     };
 
     queryData();
-  }, [activeCollection]);
+  }, [activeCollection, accessGranted]);
+
+  const grantAccess = (e) => {
+    e.preventDefault();
+
+    if (validCodes.includes(userInput)) {
+      setAcessGranted(true);
+    }
+  };
 
   return (
     <main>
-      <section className="table-section-admin">
-        <div className="table-title-wrapper-admin">
-          <h1>
-            Active collection: <span>{activeCollection}</span>
-          </h1>
+      {!accessGranted ? (
+        <div>
+          <h1>admin signup</h1>
+          <form onSubmit={grantAccess}>
+            <input
+              onChange={(e) => {
+                setUserInput(e.target.value);
+              }}
+              value={userInput}
+              placeholder="code"
+            />
+            <button>Sign in</button>
+          </form>
         </div>
+      ) : (
+        <section className="table-section-admin">
+          <div className="table-title-wrapper-admin">
+            <h1>
+              Active collection: <span>{activeCollection}</span>
+            </h1>
+          </div>
 
-        <Table data={tableData} activeCollection={activeCollection} />
-
-        <Modal data={tableData} activeCollection={activeCollection} />
-
-        <Dropdown
-          activeCollection={activeCollection}
-          setActiveCollection={setActiveCollection}
-        />
-      </section>
+          <Table data={tableData} activeCollection={activeCollection} />
+          <Modal data={tableData} activeCollection={activeCollection} />
+          <Dropdown
+            activeCollection={activeCollection}
+            setActiveCollection={setActiveCollection}
+          />
+        </section>
+      )}
     </main>
   );
 };
